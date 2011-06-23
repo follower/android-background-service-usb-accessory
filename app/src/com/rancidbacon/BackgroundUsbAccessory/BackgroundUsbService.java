0524ce0cf0705fabf275430ba1ec00c1a6e3a78b
follower <follower@rancidbacon.com>
2011-06-23 17:52:58 +1200

    Add skeleton of service with associated manifest entry.

---
+++ app/src/com/rancidbacon/BackgroundUsbAccessory/BackgroundUsbService.java
@@ -0,0 +1,18 @@
+package com.rancidbacon.BackgroundUsbAccessory;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class BackgroundUsbService extends IntentService {
+
+	public BackgroundUsbService() {
+		super("BackgroundUsbService");
+	}
+
+	@Override
+	protected void onHandleIntent(Intent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
