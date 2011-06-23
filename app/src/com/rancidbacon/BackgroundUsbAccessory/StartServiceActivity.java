697bd03d618eeaf8ad2c796a41c49128b998bfbd
follower <follower@rancidbacon.com>
2011-06-23 18:14:56 +1200

    Add logging to entry/exit of service start activity. Start the service. Exit activity.

--- app/src/com/rancidbacon/BackgroundUsbAccessory/StartServiceActivity.java
+++ app/src/com/rancidbacon/BackgroundUsbAccessory/StartServiceActivity.java
@@ -1,13 +1,26 @@
 package com.rancidbacon.BackgroundUsbAccessory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartServiceActivity extends Activity {
     /** Called when the activity is first created. */
+    
+    private String TAG = "StartServiceActivity";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        Log.d(TAG, "onCreate entered");
+
+        Intent intent = new Intent(this, BackgroundUsbService.class);
+        startService(intent);
+		
+        finish();
+		
+        Log.d(TAG, "onCreate exited");
     }
 }
