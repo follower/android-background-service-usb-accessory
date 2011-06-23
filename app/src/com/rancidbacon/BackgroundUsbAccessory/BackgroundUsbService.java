624bd3ec05feff16039bf74a8c2e096207feb1c2
follower <follower@rancidbacon.com>
2011-06-23 19:20:05 +1200

    Add method to create a notification suitable for use with the start foreground method.

--- app/src/com/rancidbacon/BackgroundUsbAccessory/BackgroundUsbService.java
+++ app/src/com/rancidbacon/BackgroundUsbAccessory/BackgroundUsbService.java
@@ -1,6 +1,9 @@
 package com.rancidbacon.BackgroundUsbAccessory;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
@@ -8,10 +11,31 @@ public class BackgroundUsbService extends IntentService {
 
     private String TAG = "BackgroundUsbService";
 
+	private static final int NOTIFICATION_ID = 1;    
+    
 	public BackgroundUsbService() {
 		super("BackgroundUsbService");
 	}
 
+	Notification getNotification() {
+
+		Notification notification = new Notification(android.R.drawable.ic_menu_info_details,
+				"Accessory connected", System.currentTimeMillis());
+		
+		Context context = getApplicationContext();
+		CharSequence contentTitle = "USB Accessory connected";
+		CharSequence contentText = "Background USB Demo Accessory connected";
+
+		// This can be changed if we want to launch an activity when notification clicked
+		Intent notificationIntent = new Intent();
+		
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);		
+		
+		return notification;
+	}
+	
 	@Override
 	protected void onHandleIntent(Intent arg0) {
 
